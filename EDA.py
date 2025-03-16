0b18115fa0b43a647a38ee3137d1838fc352575e
akshay kesarkar <118303434+Akshaykesarkar@users.noreply.github.com>
2025-03-16 23:58:35 +0530

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -88,6 +88,7 @@ def visualize_data_with_groq(client, df):
            - Distribution analysis (histograms)
            - Categorical breakdowns (bar/pie)
            - Hourly patterns (heatmaps)
+           - Include many other visualization
         4. Create 8-9 different chart/plots types focusing on these relationships
         5. OUTPUT FORMAT:
            - Only Python code within ```python blocks
